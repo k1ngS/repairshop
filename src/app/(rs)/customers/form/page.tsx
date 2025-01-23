7f3a8efe1665ac457b53071f4f4a8e9b1a1d97f7
k1ngS <marcos.beltrao@outlook.pt>
2025-01-23 15:42:50 -0300

    :sparkles: implement CustomerForm integration for editing and creating customer details

--- src/app/(rs)/customers/form/page.tsx
+++ src/app/(rs)/customers/form/page.tsx
@@ -1,36 +1,41 @@
-import { getCustomer } from "@/lib/queries/getCustomer";
 import { BackButton } from "@/components/BackButton";
+import { getCustomer } from "@/lib/queries/getCustomer";
 import * as Sentry from "@sentry/nextjs";
+import CustomerForm from "./CustomerForm";
 
 export default async function CustomerFormPage({
-  searchParams,
+	searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | undefined }>
+	searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
-  try {
-    const { customerId } = await searchParams
+	try {
+		const { customerId } = await searchParams;
 
-    // Edit customer form
-    if (customerId) {
-      const customer = await getCustomer(parseInt(customerId))
+		// Edit customer form
+		if (customerId) {
+			const customer = await getCustomer(Number.parseInt(customerId));
 
-      if (!customer) {
-        return (
-          <>
-            <h2 className="text-2xl mb-2">Customer ID #{customerId} not found</h2>
-            <BackButton title="Go Back" variant="default" />
-          </>
-        )
-      }
-      console.log(customer)
-      // Put customer form
-    } else {
-      // New customer form
-    }
-  } catch (e) {
-    if (e instanceof Error) {
-      Sentry.captureException(e)
-      throw e
-    }
-  }
+			if (!customer) {
+				return (
+					<>
+						<h2 className="text-2xl mb-2">
+							Customer ID #{customerId} not found
+						</h2>
+						<BackButton title="Go Back" variant="default" />
+					</>
+				);
+			}
+			console.log(customer);
+			// Put customer form
+      return <CustomerForm customer={customer} />
+		} else {
+			// New customer
+			return <CustomerForm />
+		}
+	} catch (e) {
+		if (e instanceof Error) {
+			Sentry.captureException(e);
+			throw e;
+		}
+	}
 }
