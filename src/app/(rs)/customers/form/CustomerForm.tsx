356c6b9b4719688eae47488b2a8c6f638bcd476b
k1ngS <marcos.beltrao@outlook.pt>
2025-01-28 10:39:30 -0300

    :bug: fix default values assignment in CustomerForm to use nullish coalescing

--- src/app/(rs)/customers/form/CustomerForm.tsx
+++ src/app/(rs)/customers/form/CustomerForm.tsx
@@ -13,17 +13,17 @@ type Props = {
 
 export default function CustomerForm({ customer }: Props) {
   const defaultValues: insertCustomerSchemaType = {
-    id: customer?.id || 0,
-    firstName: customer?.firstName || "",
-    lastName: customer?.lastName || "",
-    address1: customer?.address1 || "",
-    address2: customer?.address2 || "",
-    city: customer?.city || "",
-    state: customer?.state || "",
-    zip: customer?.zip || "",
-    phone: customer?.phone || "",
-    email: customer?.email || "",
-    notes: customer?.notes || "",
+    id: customer?.id ?? 0,
+    firstName: customer?.firstName ?? "",
+    lastName: customer?.lastName ?? "",
+    address1: customer?.address1 ?? "",
+    address2: customer?.address2 ?? "",
+    city: customer?.city ?? "",
+    state: customer?.state ?? "",
+    zip: customer?.zip ?? "",
+    phone: customer?.phone ?? "",
+    email: customer?.email ?? "",
+    notes: customer?.notes ?? "",
   }
 
   const form = useForm<insertCustomerSchemaType>({
